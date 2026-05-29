282a986bb2f866d06d9e38abaf7a1f709740cc69
2052202 <s2052202@online.houstonisd.org>
2026-05-29 04:53:37 +0000

    push

---
+++ main/fastMult.cpp
@@ -0,0 +1,5 @@
+#include "fastMult.hpp"
+
+long long pow(long long base, long long power, long long mod){
+    
+}
---
+++ main/fastMult.hpp
@@ -0,0 +1,8 @@
+#ifndef fastMult_HPP
+#define fastMult_HPP
+
+int getBit()
+
+long long pow(long long base, long long power, long long mod);
+
+#endif
