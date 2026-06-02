5f28702a107b2201f4cb953167b7d9a36c9b8db6
2052202 <s2052202@online.houstonisd.org>
2026-06-02 15:44:15 +0000

    Deleting Libraries

--- main/libraries/ModularInverse/ModularInverse.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef fastMult_HPP
-#define fastMult_HPP
-
-
-
-long long MultModInverse(long long a, long long n);
-
-#endif
--- main/libraries/preload/preload.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef fastMult_HPP
-#define fastMult_HPP
-
-
-
-bool confirm(long long p, long long q, long long e, long long phi_n, long long d);
-
-#endif
